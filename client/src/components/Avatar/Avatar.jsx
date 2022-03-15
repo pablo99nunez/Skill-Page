7ec5435cb9b886a8d83bf18536841e993890f1c5
pablo99nunez <pablo99nunez@gmail.com>
2022-03-15 20:41:51 -0300

    Initialize repository

---
+++ client/src/components/Avatar/Avatar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Hexagon from "../Hexagon/Hexagon";
+import "./Avatar.scss";
+
+export default function Avatar() {
+  return (
+    <div>
+      <Hexagon></Hexagon>
+    </div>
+  );
+}
